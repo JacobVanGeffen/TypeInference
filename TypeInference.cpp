3e31507eaecaa6dba2af2f96c50c5b2945ba2c67
Jacob Van Geffen <jsv@cs.utexas.edu>
2015-11-22 16:47:32 -0600

    compiles now

--- TypeInference.cpp
+++ TypeInference.cpp
@@ -5,23 +5,22 @@
 #include "ast/FunctionType.h"
 #include "ast/expression.h"
 #include "TypeInference.h"
+#include <functional>
 
 int typeVarCount = 0;
-
+void eval_poop(Expression* b, Expression* eval_e1, Expression* eval_e2, string x, const std::function<void(int)>& asdf) {}
 void report_error(Expression* e, const string & s) {
 	cout << "Run-time error in expression " << e->to_value() << endl;
 	cout << s << endl;
 	exit(1);
 }
 
-/*
-Evaluator::Evaluator() {
-	sym_tab.push();
-	c = 0;
+Expression* get_wrapper(Type* type) {
+	Expression* wrapper = AstInt::make(0);
+	wrapper->type = type;
+	return wrapper;
 }
-*/
 
-/*
 Expression* TypeInference::eval_unop(AstUnOp* b) {
 	Expression* e = b->get_expression();
 	Expression* eval_e = eval(e);
@@ -189,7 +188,7 @@ Expression* TypeInference::eval_binop(AstBinOp* b) {
 		}
 	}
 }
-*/
+/**/
 
 Expression* TypeInference::eval(Expression* e) {
 	Expression* res_exp = NULL;
@@ -292,8 +291,7 @@ Expression* TypeInference::eval(Expression* e) {
 			Type* id_var = VariableType::make("v" + typeVarCount);
 			typeVarCount++;
 
-			Expression* type_wrapper = AstInt::make(0);
-			type_wrapper->type = id_var;
+			Expression* type_wrapper = get_wrapper(id_var);
 
 			// Need to push/pop? (almost def not) (aka sam thinks not)
 			sym_tab.add(formal, type_wrapper);
@@ -372,49 +370,5 @@ Expression* TypeInference::eval(Expression* e) {
 
 TypeInference::TypeInference(Expression * e) {
 // CODE GOES HERE
+	sym_tab.push();
 }
-
-/*
-this->program = e;
-
-Type* t1 = ConstantType::make("Int");
-Type* t2 = ConstantType::make("String");
-Type* t3 = ConstantType::make("Int");
-Type* var1 = VariableType::make("x");
-cout << t1->to_string() << " " << t1 << endl;
-cout << t2->to_string() << " " << t2 << endl;
-cout << t3->to_string() << " " << t3 << endl;
-vector<Type*> v1;
-v1.push_back(var1);
-v1.push_back(t2);
-
-vector<Type*> v2;
-v2.push_back(t3);
-v2.push_back(t2);
-
-Type* t4 = FunctionType::make("fun", v1);
-Type* t5 = FunctionType::make("fun", v2);
-cout << t4->to_string() << " " << t4 << endl;
-cout << t5->to_string() << " " << t5 << endl;
-
-//prints all types & reps in table
-Type::print_all_types();
-
-Type* t1 = t4;
-Type* t2 = t5;
-
-cout << "Type 1:" << t1->to_string() << "Rep: " << t1->find()->to_string() << endl;
-cout <<	"Type 2:" << t2->to_string() << "Rep: " << t2->find()->to_string() << endl;
-
-cout << "unify: " << t1->unify(t2) << endl;
-
-cout << "Type 1:" << t1->to_string() << "Rep: " << t1->find()->to_string() << endl;
-cout <<	"Type 2:" << t2->to_string() << "Rep: " << t2->find()->to_string() << endl;
-
-cout << "Type 1:" << t1->to_string() << "Rep: " << t1->find()->to_string() << endl;
-cout <<	"Type 2:" << var1->to_string() << "Rep: " << var1->find()->to_string() << endl;
-
-
-Type::print_all_types();
-*/
-
